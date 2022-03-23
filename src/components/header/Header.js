2449b098e51cb622098e872c3ebf6a8d76f4fd05
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-03-23 19:58:35 +0100

    1st version responsive header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,47 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
+import {FaBars} from 'react-icons/fa'
+
 
 export const Header = () => {
+    const [isOpen, setIsOpen] = useState(false)
   return (
     <>
     <Nav>
-    <NavMenuLeft className='NavMenuLeft'>
+    {/* <NavMenuLeft className='NavMenuLeft'> */}
       
       <Link  href="/" className='logo-img'>
         <Logo src="/logo.png" alt="" ></Logo> 
       </Link>
       
-    </NavMenuLeft>
-        
-        <NavMenuCenter className='NavMenuCenter'> 
+    {/* </NavMenuLeft> */}
 
-            <Link href="/">
-                <span>Inicio</span>
-            </Link>
+    <Bars onClick= {() => setIsOpen(!isOpen)}>
+
+        <span />
+        <span />
+        <span />
+    </Bars>
+        
+    <NavMenuCenter isOpen= {isOpen} className='NavMenuCenter'> 
+
+        <Link href="/">
+            <span>Inicio</span>
+        </Link>
+    
+        <Link href="/quienes-somos">
+            <span>Quiénes somos</span>
+        </Link>
+        <Link href="/formacion">
+            <span>Formación</span>
+        </Link>
+        <Link href="/login">
+            <span>CONÉCTATE</span>
+        </Link>    
        
-            <Link href="/quienes-somos">
-                <span>Quiénes somos</span>
-            </Link>
-            <Link href="/formacion">
-                <span>Formación</span>
-            </Link>
-            {/* <a href="">
-                <span>Equipo</span>
-            </a>
-            <a href="">
-                <span>Contacto</span>
-            </a> */}
-
-        </NavMenuCenter>
+    </NavMenuCenter>
         
-        <NavMenuRight>
-            
-            <Link href="/login">
-                <span>CONÉCTATE</span>
-            </Link>                
-            
+    {/* <NavMenuRight>
+        
+                    
+        
 
-        </NavMenuRight>
+    </NavMenuRight> */}
     </Nav>
     </>
   )
@@ -59,14 +64,41 @@ display:flex;
 justify-content: space-between;
 align-items: center;
 z-index: 3;
+flex-wrap: wrap;
 `
 // const Logo = styled.img`
 // `
 
-const NavMenuLeft= styled.div`
-display:flex;
-justify-content: center;
-align-items: center;
+// const NavMenuLeft= styled.div`
+// display:flex;
+// justify-content: center;
+// align-items: center;
+
+
+// `
+
+const Bars = styled(FaBars) `
+
+display: none;
+color: #fff;
+cursor: pointer;
+span {
+    height: 2px;
+    width: 25px;
+    background-color: #7b7fda;
+    margin-bottom: 4px;
+    border-radius: 5px;
+  }
+@media screen and (max-width: 768px) {
+  display: block;
+  position: absolute;
+  top: 0;
+  right: 0;
+  transform: translate(-100%, 75%);
+  font-size: 1.8rem;
+  
+// display:flex;
+}
 
 `
 
@@ -82,45 +114,65 @@ display: flex;
 img {
     display:block;
     width: 100%;
+    
 }
 `
 
 const Link = styled.a`
+
 `
 
 const NavMenuCenter= styled.div`
 display: flex;
 align-items: center;
-justify-content: center;
+justify-content: space-between;
 color: #3c096c;
-width: 40%;
-a{
-    text-decoration: none;
-    margin: 20px;
-    display: flex;
-    // justify-content: space-around;
-    align-items: center;
-}
+position: relative;
+// margin-top:20px;
+
+// a{
+//     text-decoration: none;
+//     margin: 20px;
+//     display: flex;
+//     // justify-content: space-around;
+//     align-items: center;
+// }
+
+@media screen and (max-width: 768px) {
+    // display: none;
+    overflow: hidden;
+    // display: flex;
+    flex-direction: column;
+    // top: 50;
+    max-height: ${({ isOpen }) => (isOpen ? "300px" : "0")};
+    transition: max-height 0.3s ease-in;
+    background-color:red;
+    width: 100%;
+  }
 `
 
-const NavMenuRight= styled.div`
-display: flex;
-justify-content: flex-end;
-color: #3c096c;
-font-weight: bold;
+// const NavMenuRight= styled.div`
+// display: flex;
+// justify-content: flex-end;
+// color: #3c096c;
+// font-weight: bold;
 
 
-a{
-    text-decoration: none;
-    margin-right: 20px;
-    background-color: #e0cdcd;
-    border-radius: 14px;
-    padding:10px;
-    position: relative;
-    &:hover{
-    background-color: #3c096c;
-    color: #e0cdcd;
-    }
+// a{
+//     text-decoration: none;
+//     margin-right: 20px;
+//     background-color: #e0cdcd;
+//     border-radius: 14px;
+//     padding:10px;
+//     position: relative;
+//     &:hover{
+//     background-color: #3c096c;
+//     color: #e0cdcd;
+//     }
+
     
-}
-`
+// }
+
+// @media screen and (max-width: 768px) {
+//     display: none;
+//   }
