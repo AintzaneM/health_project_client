f3c23cff0ac566dd3709f1ce16e94e9052527844
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-03-24 11:41:57 +0100

    responsive navbar styled

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,53 +1,42 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import {FaBars} from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa'
 
 
 export const Header = () => {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <>
-    <Nav>
-    {/* <NavMenuLeft className='NavMenuLeft'> */}
-      
-      <Link  href="/" className='logo-img'>
-        <Logo src="/logo.png" alt="" ></Logo> 
-      </Link>
-      
-    {/* </NavMenuLeft> */}
+      <Nav>
 
-    <Bars onClick= {() => setIsOpen(!isOpen)}>
+        <LogoContainer>
+        <a href="/" className='logo-img'>
+          <Logo src="/logo.png" alt="" ></Logo>
+        </a>
+        </LogoContainer>
 
-        <span />
-        <span />
-        <span />
-    </Bars>
-        
-    <NavMenuCenter isOpen= {isOpen} className='NavMenuCenter'> 
+        <Bars onClick={() => setIsOpen(!isOpen)}>
+          <span />
+          <span />
+          <span />
+        </Bars>
 
-        <Link href="/">
+        <NavMenuCenter isOpen={isOpen} className='NavMenuCenter'>
+
+          <Link href="/">
             <span>Inicio</span>
-        </Link>
-    
-        <Link href="/quienes-somos">
+          </Link>
+          <Link href="/quienes-somos">
             <span>Quiénes somos</span>
-        </Link>
-        <Link href="/formacion">
+          </Link>
+          <Link href="/formacion">
             <span>Formación</span>
-        </Link>
-        <Link href="/login">
+          </Link>
+          <Link href="/login">
             <span>CONÉCTATE</span>
-        </Link>    
-       
-    </NavMenuCenter>
-        
-    {/* <NavMenuRight>
-        
-                    
-        
-
-    </NavMenuRight> */}
-    </Nav>
+          </Link>
+        </NavMenuCenter>
+      </Nav>
     </>
   )
 }
@@ -57,38 +46,24 @@ position: sticky;
 top: 0;
 left: 0;
 right: 0;
-height: 70px;
+height: 80px;
 width: 100%;
-background-color:#ffc05c;
+background-color:#bfd8bd;
 display:flex;
+flex-direction: row;
 justify-content: space-between;
 align-items: center;
 z-index: 3;
 flex-wrap: wrap;
 `
-// const Logo = styled.img`
-// `
-
-// const NavMenuLeft= styled.div`
-// display:flex;
-// justify-content: center;
-// align-items: center;
-
-
-// `
-
 const Bars = styled(FaBars) `
-
 display: none;
-color: #fff;
+color: #3c1642;
 cursor: pointer;
-span {
-    height: 2px;
-    width: 25px;
-    background-color: #7b7fda;
-    margin-bottom: 4px;
-    border-radius: 5px;
-  }
+
+  
+
+
 @media screen and (max-width: 768px) {
   display: block;
   position: absolute;
@@ -96,58 +71,65 @@ span {
   right: 0;
   transform: translate(-100%, 75%);
   font-size: 1.8rem;
-  
-// display:flex;
 }
 
+`
+const LogoContainer =styled.div`
+display:flex;
+justify-content: center;
+align-items: center;
+position: relative;
+cursor: pointer;
 `
 
 const Logo = styled.img`
-padding: 0;
-width: 60px;
-// margin-top: 4px;
-margin-left: 20px;
+margin: 10px 20px;
 max-height: 60px;
-font-size: 24px;
-display: flex;
-
-img {
-    display:block;
-    width: 100%;
-    
-}
+display:block;
+width: 100%;
 `
 
 const Link = styled.a`
+text-decoration: none;
+display: flex;
+justify-content: space-around;
+align-items: center;
+margin-right: 20px;
+color: #3c1642;
+border-bottom: solid;
+transition: all 0.3s ease-in-out;
+&:hover {
+  border-bottom: 5px solid #edeec9;
+}
 
+@media screen and (max-width: 768px) {
+//  padding: 20px;
+ margin: 20px 0px;
+ display: flex;
+ align-items: center;
+ justify-content: center;
+ border-bottom: solid;
+}
 `
-
 const NavMenuCenter= styled.div`
 display: flex;
 align-items: center;
 justify-content: space-between;
 color: #3c096c;
 position: relative;
-// margin-top:20px;
 
-// a{
-//     text-decoration: none;
-//     margin: 20px;
-//     display: flex;
-//     // justify-content: space-around;
-//     align-items: center;
-// }
 
 @media screen and (max-width: 768px) {
-    // display: none;
+    
     overflow: hidden;
-    // display: flex;
+    display: flex;
     flex-direction: column;
-    // top: 50;
     max-height: ${({ isOpen }) => (isOpen ? "300px" : "0")};
     transition: max-height 0.3s ease-in;
-    background-color:red;
+    background-color: #bfd8bd;
     width: 100%;
+   
+    
   }
 `
 
